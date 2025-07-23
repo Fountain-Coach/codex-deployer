2ced714bc8f8b3ef436dce39ff60d3cf77b4c13c
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-07-23 07:47:17 +0200

    Add ScreenplayGUI package

---
+++ repos/fountainai/ScreenplayGUI/Tests/ScreenplayGUITests/ScreenplayGUITests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import ScreenplayGUI
+
+final class ScreenplayGUITests: XCTestCase {
+    func testParserProducesNodes() {
+        let script = """
+        INT. LAB - DAY
+        AI
+        Testing parser
+        """
+        let parser = FountainParser()
+        let nodes = parser.parse(script)
+        XCTAssertFalse(nodes.isEmpty)
+    }
+}
