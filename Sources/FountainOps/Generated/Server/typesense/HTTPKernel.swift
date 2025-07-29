030c5eb8fa42c02645e129d7a75d3889e3f1d9c6
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-07-29 22:43:48 +0200

    Generate Typesense client and server

---
+++ Sources/FountainOps/Generated/Server/typesense/HTTPKernel.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public struct HTTPKernel {
+    let router: Router
+
+    public init(handlers: Handlers = Handlers()) {
+        self.router = Router(handlers: handlers)
+    }
+
+    public func handle(_ request: HTTPRequest) async throws -> HTTPResponse {
+        try await router.route(request)
+    }
+}
+
+© 2025 Contexter alias Benedikt Eickhoff 🛡️ All rights reserved.
