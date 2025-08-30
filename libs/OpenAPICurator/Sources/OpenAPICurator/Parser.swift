9d09ea0f05a91852a47d2230a2ef5ad2b8679809
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-08-30 07:19:04 +0200

    Fix CoTEndpointTests to call getChatCoT

--- libs/OpenAPICurator/Sources/OpenAPICurator/Parser.swift
+++ libs/OpenAPICurator/Sources/OpenAPICurator/Parser.swift
@@ -3,6 +3,11 @@ import Foundation
 enum Parser {
     static func parse(_ specs: [Spec]) -> OpenAPI {
         let ops = specs.flatMap { $0.operations }
-        return OpenAPI(operations: ops)
+        let exts = specs.reduce(into: [String: [String: String]]()) { result, spec in
+            for (op, ext) in spec.extensions {
+                result[op, default: [:]].merge(ext) { current, _ in current }
+            }
+        }
+        return OpenAPI(operations: ops, extensions: exts)
     }
 }
